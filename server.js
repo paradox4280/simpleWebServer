63f23ff61be8e23cb1c68f04daa6728dc73af3b8
paradox4280 <zmf5c2fi@pm.me>
2022-09-19 03:26:58 +0430

    intial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import morgan from "morgan"
+import express from "express"
+import serveIndex from "serve-Index"
+
+import { PORT } from "./config/defaults.js"
+
+const app = express()
+
+app.use(morgan("tiny"))
+app.use("/", express.static("./public/files/"), serveIndex("./public/files/", {
+    icons: true, hidden: true, stylesheet:  "./public/css/style.css"
+}))
+
+console.clear()
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(
+            `server is running on http://127.0.0.1:${PORT}`
+        ))
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+
+start()
